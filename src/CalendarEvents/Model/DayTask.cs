faf239c9f61acc7de7e77fe29f9b8210be423965
Валентин Борнашов <shipverty@vk.com>
2023-07-08 00:38:49 +0700

    feature(DayTask): класс с задачами

---
+++ src/CalendarEvents/Model/DayTask.cs
@@ -0,0 +1,27 @@
+﻿namespace Model;
+
+/// <summary>
+/// Хранит информацию о задаче.
+/// </summary>
+public class DayTask : IDayEvent
+{
+    /// <summary>
+    /// Создает экземпляр класса <see cref="DayTask"/>.
+    /// </summary>
+    /// <param name="title">Название.</param>
+    public DayTask(string title)
+    {
+        Title = title;
+        IsDone = false;
+    }
+    
+    /// <summary>
+    /// Возвращает и задает название события.
+    /// </summary>
+    public string Title { get; set; }
+    
+    /// <summary>
+    /// Хранит значение, указывающее, выполнена задача или нет.
+    /// </summary>
+    public bool IsDone { get; set; }
+}
