a5032efadca8890441ad917a13608534a11bbfb2
Валентин Борнашов <shipverty@vk.com>
2023-07-28 13:17:02 +0700

    feature(MainVM): добавлена реализация десериализации и сериализации данных.

--- src/CalendarEvents/ViewModel/MainVM.cs
+++ src/CalendarEvents/ViewModel/MainVM.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Model;
 using ViewModel.Services;
 
 namespace ViewModel;
@@ -9,11 +11,22 @@ namespace ViewModel;
 public class MainVM : ObservableObject
 {
     /// <summary>
+    /// Путь до сериализации.
+    /// </summary>
+    private readonly string _path = 
+        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        + @"/CalendarEvents/Tasks.json";
+
+    private readonly EventRepository _repository;
+    
+    /// <summary>
     /// Создает экземпляр класса <see cref="MainVM" />.
     /// </summary>
     /// <param name="navigationService">Сервис навигации пользовательских элементов управления.</param>
-    public MainVM(INavigationService navigationService)
+    public MainVM(INavigationService navigationService, EventRepository repository)
     {
+        _repository = repository;
+        repository.Events = Serializer.Deserialize(_path);
         NavigationService = navigationService;
         NavigationService.NavigateTo<CalendarVM>();
     }
@@ -22,4 +35,9 @@ public class MainVM : ObservableObject
     /// Возвращает и задает сервис навигации пользовательских элементов управления.
     /// </summary>
     public INavigationService NavigationService { get; set; }
+    
+    public void OnWindowClosing(object sender, CancelEventArgs e)
+    {
+        Serializer.Serialize(_repository.Events, _path);
+    }
 }
