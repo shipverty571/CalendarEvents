5081257586fd072eb3842b617782944395a03a90
Валентин Борнашов <shipverty@vk.com>
2023-08-05 13:23:46 +0700

    feature(EventsManagementVM): коллекция с цветами инициализируется перечислением Colors

--- src/CalendarEvents/ViewModel/EventsManagementVM.cs
+++ src/CalendarEvents/ViewModel/EventsManagementVM.cs
@@ -1,7 +1,9 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel;
+using System.Drawing;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Model;
+using Model.Enums;
 using ViewModel.Services;
 
 namespace ViewModel;
@@ -20,16 +22,11 @@ public class EventsManagementVM : ObservableObject, IDialogResultVMHelper
         EventRepository = eventRepository;
         AddEventCommand = new RelayCommand(AddEvent);
         CloseCommand = new RelayCommand(Close);
-        Colors = new List<string>
-        {
-            "LightGray",
-            "Red",
-            "Orange",
-            "Yellow",
-            "Green",
-            "Blue",
-            "#D7D7D7"
-        };
+        var colorDesctiptions = typeof(Colors).GetMembers()
+            .SelectMany(member => member.GetCustomAttributes(typeof (DescriptionAttribute), true).Cast<DescriptionAttribute>())
+            .ToList();
+
+        Colors = colorDesctiptions.Select(color => color.Description).ToList();
     }
 
     /// <summary>
