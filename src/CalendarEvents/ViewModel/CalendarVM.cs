e0cfd68e495934cebff990bcbda847b59544cef5
Валентин Борнашов <shipverty@vk.com>
2023-08-07 16:36:28 +0700

    refactor(CalendarVM): убран NavigationService

--- src/CalendarEvents/ViewModel/CalendarVM.cs
+++ src/CalendarEvents/ViewModel/CalendarVM.cs
@@ -49,12 +49,10 @@ public class CalendarVM : ObservableObject
     /// <param name="eventRepository">Хранилище задач.</param>
     /// <param name="viewModelFactory">Фабрика ViewModel.</param>
     public CalendarVM(
-        INavigationService navigationService,
         IDialogService dialogService,
         EventRepository eventRepository,
         Func<Type, ObservableObject> viewModelFactory)
     {
-        NavigationService = navigationService;
         DialogService = dialogService;
         EventRepository = eventRepository;
         ViewModelFactory = viewModelFactory;
@@ -94,16 +92,10 @@ public class CalendarVM : ObservableObject
         }
     }
 
-
     /// <summary>
     /// Возвращает и задает сервис диалоговых окон.
     /// </summary>
     public IDialogService DialogService { get; set; }
-
-    /// <summary>
-    /// Возвращает и задает сервис навигации пользовательских элементов управления.
-    /// </summary>
-    public INavigationService NavigationService { get; set; }
 
     /// <summary>
     /// Возвращает команду смены месяца на следующий.
@@ -114,7 +106,7 @@ public class CalendarVM : ObservableObject
     /// Возвращает команду смены месяца на предыдущий.
     /// </summary>
     public RelayCommand SelectPrevMonth { get; }
-    
+
     /// <summary>
     /// Возвращает команду закрытия диалогового окна.
     /// </summary>
