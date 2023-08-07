0f0bde38256ced8edbb4a0b08c7b9e124def9905
Валентин Борнашов <shipverty@vk.com>
2023-08-07 11:41:55 +0700

    feature(MessageControl, MessageVM): добавлен контрол для показа сообщений

---
+++ src/CalendarEvents/ViewModel/MessageVM.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ViewModel.Services;
+
+namespace ViewModel;
+
+public class MessageVM : ObservableObject, IDialogResultVMHelper
+{
+    public MessageVM()
+    {
+        CloseCommand = new RelayCommand(Close);
+        OKCommand = new RelayCommand(OK);
+    }
+    
+    public string Text { get; set; }
+    
+    public RelayCommand CloseCommand { get; }
+    
+    public RelayCommand OKCommand { get; }
+    
+    public event EventHandler<RequestCloseDialogEventArgs> RequestCloseDialog;
+    
+    private void Close()
+    {
+        InvokeRequestCloseDialog(
+            new RequestCloseDialogEventArgs(false));
+    }
+
+    private void OK()
+    {
+        InvokeRequestCloseDialog(
+            new RequestCloseDialogEventArgs(true));
+    }
+    
+    private void InvokeRequestCloseDialog(RequestCloseDialogEventArgs e)
+    {
+        RequestCloseDialog.Invoke(this, e);
+    }
+    
+}
